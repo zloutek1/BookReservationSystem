ba3e2926a283089c9cad561f097a776c0abbeb94
Tomáš Ljutenko <zloutek1@gmail.com>
2022-10-01 22:45:04 +0200

    added basic seeding

---
+++ BookReservationSystemDAL/Data/BookReservationSystemDBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using BookReservationSystemDAL.Models;
+
+namespace BookReservationSystemDAL.Data
+{
+    public class BookReservationSystemDBContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Reservation> Reservation { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies();
+        }
+
+        // https://docs.microsoft.com/en-us/ef/core/modeling/
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Seed();
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
+    }
+}
