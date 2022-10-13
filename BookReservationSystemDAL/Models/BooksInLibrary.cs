470186fe9b7790600626f2cc72ab402f9958be0b
Tomáš Ljutenko <zloutek1@gmail.com>
2022-10-13 16:00:41 +0200

    feat: added ability for library to have same book multiple times

---
+++ BookReservationSystemDAL/Models/BooksInLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookReservationSystemDAL.Models;
+
+public class BooksInLibrary: BaseEntity
+{
+    public Guid BookId { get; set; }
+    
+    [ForeignKey(nameof(BookId))]
+    public virtual Book Book { get; set; }
+
+    public Guid LibraryId { get; set; }
+    
+    [ForeignKey(nameof(LibraryId))]
+    public virtual Library Library { get; set; }
+    
+    public int Count { get; set; }
+}
