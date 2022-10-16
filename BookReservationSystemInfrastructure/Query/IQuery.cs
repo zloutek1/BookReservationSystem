014d7d75da48de7991f81e212b9e93068a58602a
Tomáš Ljutenko <zloutek1@gmail.com>
2022-10-16 21:21:43 +0200

    Moved EF implementation to separate project

---
+++ BookReservationSystemInfrastructure/Query/IQuery.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace BookReservationSystemInfrastructure.Query;
+
+public interface IQuery<TEntity> where TEntity : class, new()
+{
+    /// <summary>
+    /// Adds a possibility to filter the result
+    /// </summary>
+    IQuery<TEntity> Where<T>(Expression<Func<T, bool>> rootPredicate, string columnName) where T : IComparable<T>;
+
+    /// <summary>
+    /// Adds a specified sort criteria to the query.
+    /// </summary>
+    IQuery<TEntity> OrderBy<T>(string columnName, bool ascendingOrder = true) where T : IComparable<T>;
+
+    /// <summary>
+    /// Adds a possibility to paginate the result
+    /// </summary>
+    IQuery<TEntity> Page(int pageToFetch, int pageSize = 10);
+
+    /// <summary>
+    /// Executes the query and returns the results.
+    /// </summary>
+    IEnumerable<TEntity> Execute();
+}
