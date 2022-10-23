38a320cc441989f83b7f7e0c0eb92bdb09968743
Tamara Babálová <xbabalov@fi.muni.cz>
2022-10-23 17:49:37 +0200

    UOW separated but wrong interface dependency

---
+++ BookReservationSystemInfrastructure.EFCore/UnitOfWork/AuthorUOW.cs
@@ -0,0 +1,43 @@
+﻿using BookReservationSystemDAL.Data;
+using BookReservationSystemDAL.Models;
+using BookReservationSystemInfrastructure.EFCore.Repository;
+using BookReservationSystemInfrastructure.Repository;
+using BookReservationSystemInfrastructure.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookReservationSystemInfrastructure.EFCore.UnitOfWork
+{
+    public class AuthorUOW : IUnitOfWork
+    {
+        private readonly BookReservationSystemDbContext _context;
+        private IRepository<Author>? _authorRepository;
+        private IRepository<Book>? _bookRepository;
+
+        public AuthorUOW()
+        {
+            _context = new BookReservationSystemDbContext();
+        }
+
+        public AuthorUOW(BookReservationSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Author> AuthorRepository => _authorRepository ??= new GenericRepository<Author>(_context);
+        public IRepository<Book> BookRepository => _bookRepository ??= new GenericRepository<Book>(_context);
+
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
