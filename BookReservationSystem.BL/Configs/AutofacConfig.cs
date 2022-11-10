4f518d1dabad089d63b39bfb81e6de0f2c667638
Tomáš Ljutenko <zloutek1@gmail.com>
2022-11-10 17:06:14 +0100

    prepare AutoMapper and AutoFac configs

---
+++ BookReservationSystem.BL/Configs/AutofacConfig.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using AutoMapper;
+
+namespace BookReservationSystem.BL.Configs;
+
+public class AutofacConfig
+{
+    public static IContainer Configure()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterInstance(new Mapper(new MapperConfiguration(AutoMapperConfig.ConfigureMapping)))
+            .As<IMapper>()
+            .SingleInstance();
+
+        return builder.Build();
+    }
+}
