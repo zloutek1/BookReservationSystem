2714cea0c1fe96db1e614fb8f730ae142c7df2c1
Tomáš Ljutenko <zloutek1@gmail.com>
2022-12-16 13:20:12 +0100

    feat: added service exceptions

---
+++ BookReservationSystem.BL/Exceptions/NotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace BookReservationSystem.BL.Exceptions;
+
+public class NotFoundException: ServiceException
+{
+    public NotFoundException()
+    {
+    }
+
+    protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public NotFoundException(string? message) : base(message)
+    {
+    }
+
+    public NotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ BookReservationSystem.BL/Exceptions/ServiceException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace BookReservationSystem.BL.Exceptions;
+
+public class ServiceException: Exception
+{
+    public ServiceException()
+    {
+    }
+
+    protected ServiceException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public ServiceException(string? message) : base(message)
+    {
+    }
+
+    public ServiceException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
