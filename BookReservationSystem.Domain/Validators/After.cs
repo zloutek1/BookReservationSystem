d66f86418bafa70038f49f53fe1f96e0ab5e0af3
Tomáš Ljutenko <zloutek1@gmail.com>
2022-12-16 13:19:42 +0100

    feat: added custom date validation

---
+++ BookReservationSystem.Domain/Validators/After.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace BookReservationSystem.Domain.Validators;
+
+public class After: ValidationAttribute
+{
+    private string OtherProperty { get; }
+
+    public After(string otherProperty)
+    {
+        OtherProperty = otherProperty ?? throw new ArgumentNullException(nameof(otherProperty));
+    }
+    
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var otherPropertyInfo = validationContext.ObjectType.GetRuntimeProperty(OtherProperty);
+        if (otherPropertyInfo == null)
+        {
+            return new ValidationResult($"Unknown property {OtherProperty}");
+        }
+        if (otherPropertyInfo.GetIndexParameters().Length > 0)
+        {
+            throw new ArgumentException($"Property not found {OtherProperty}");
+        }
+        var otherValue = otherPropertyInfo.GetValue(validationContext.ObjectInstance, null);
+
+        var dateTime = Convert.ToDateTime(value);
+        var otherDateTime = Convert.ToDateTime(otherValue);        
+        
+        return dateTime > otherDateTime 
+            ? ValidationResult.Success 
+            : new ValidationResult($"{validationContext.DisplayName} must be after {GetDisplayNameForProperty(otherPropertyInfo)}");
+    }
+    
+    private string? GetDisplayNameForProperty(MemberInfo property)
+    {
+        var attributes = CustomAttributeExtensions.GetCustomAttributes(property, true);
+        foreach (var attribute in attributes)
+        {
+            if (attribute is DisplayAttribute display)
+            {
+                return display.GetName();
+            }
+        }
+
+        return OtherProperty;
+    }
+}
---
+++ BookReservationSystem.Domain/Validators/FutureOrPresent.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookReservationSystem.Domain.Validators;
+
+public class FutureOrPresent: ValidationAttribute
+{
+    private const string DefaultErrorMessage = "Date must be now or in the future";
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var dateTime = Convert.ToDateTime(value);
+        return dateTime >= DateTime.Now 
+            ? ValidationResult.Success 
+            : new ValidationResult(ErrorMessage ?? DefaultErrorMessage);
+    }
+}
